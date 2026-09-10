23986261e67a2ca7456d66019fa95f495d2309db
Ксения Плеханова <Tyuiu.PlehanovaKA@outlook.com>
2026-09-10 22:59:09 +0500

    Task1

---
+++ Tyuiu.PlehanovaKA.Sprint0.Task0.V0/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.PlehanovaKA.Sprint0.Task0.V0
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
