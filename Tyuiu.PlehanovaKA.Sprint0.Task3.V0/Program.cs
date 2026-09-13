ac890d859ddafcdf0a636cc1f0a7616fbe90709b
Ксения Плеханова <Tyuiu.PlehanovaKA@outlook.com>
2026-09-13 23:52:38 +0500

    Task3

---
+++ Tyuiu.PlehanovaKA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PlehanovaKA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.PlehanovaKA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
