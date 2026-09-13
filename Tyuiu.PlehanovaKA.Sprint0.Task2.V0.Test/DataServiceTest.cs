93315b14fc391dd0e1fa2be15caf88af32336ce0
Ксения Плеханова <Tyuiu.PlehanovaKA@outlook.com>
2026-09-13 20:57:33 +0500

    Task2

---
+++ Tyuiu.PlehanovaKA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PlehanovaKA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PlehanovaKA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Ксения";
+            var res = DataService.GetMessage(name);
+        Assert.AreEqual("Привет..., Ксения", res);
+        }
+    }
+}
---
+++ Tyuiu.PlehanovaKA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PlehanovaKA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PlehanovaKA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Ксения"));
+            Console.ReadKey();
+        }
+    }
+}
