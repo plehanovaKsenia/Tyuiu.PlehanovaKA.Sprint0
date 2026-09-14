546e4fe535231c6e4c5d22d9fab870285b2d8ca4
Ксения Плеханова <Tyuiu.PlehanovaKA@outlook.com>
2026-09-14 21:13:57 +0500

    Task6

---
+++ Tyuiu.PlehanovaKA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.PlehanovaKA.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.PlehanovaKA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        public void CheckMultiplicationArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
