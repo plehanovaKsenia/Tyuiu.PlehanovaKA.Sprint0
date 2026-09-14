9976614194b73449e7ee4cd36944464e82d3d621
Ксения Плеханова <Tyuiu.PlehanovaKA@outlook.com>
2026-09-14 12:55:11 +0500

    Task4

---
+++ Tyuiu.PlehanovaKA.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.PlehanovaKA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.PlehanovaKA.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
