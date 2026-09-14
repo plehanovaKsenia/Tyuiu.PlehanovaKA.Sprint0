da0d2f0b74a1de0b811196c8b31a5ba609f63157
Ксения Плеханова <Tyuiu.PlehanovaKA@outlook.com>
2026-09-14 20:29:13 +0500

    Task5

---
+++ Tyuiu.PlehanovaKA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PlehanovaKA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.PlehanovaKA.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = "+DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
